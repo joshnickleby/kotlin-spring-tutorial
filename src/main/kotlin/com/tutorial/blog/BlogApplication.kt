c6373976fea90a534bba384dfaf77a15377dfdd8
exmarxdespot <ofgodsandmythos@gmail.com>
2018-08-09 18:40:43 -0500

    Some config type files

--- src/main/kotlin/com/tutorial/blog/BlogApplication.kt
+++ src/main/kotlin/com/tutorial/blog/BlogApplication.kt
@@ -1,8 +1,10 @@
 package com.tutorial.blog
 
+import com.samskivert.mustache.Mustache
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class BlogApplication
@@ -10,6 +12,12 @@ class BlogApplication
 //fun main(args: Array<String>) {
 //  runApplication<BlogApplication>(*args)
 //}
+
+/** https://spring.io/guides/tutorials/spring-boot-kotlin/ **/
+
+@Bean
+fun mustacheCompiler(loader: Mustache.TemplateLoader?) =
+    Mustache.compiler().escapeHTML(false).withLoader(loader)
 
 /** Customize app **/
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/tutorial/blog/config/MarkdownConverter.kt
@@ -0,0 +1,19 @@
+package com.tutorial.blog.config
+
+import org.commonmark.ext.autolink.AutolinkExtension
+import org.commonmark.parser.Parser
+import org.commonmark.renderer.html.HtmlRenderer
+import org.springframework.stereotype.Service
+
+@Service
+class MarkdownConverter : (String?) -> String {
+
+  private val parser = Parser.builder().extensions(listOf(AutolinkExtension.create()))
+  private val renderer = HtmlRenderer.builder().build()
+
+  override fun invoke(input: String?): String {
+    if (input == null || input == "") { return "" }
+
+    return renderer.render(parser.build().parse(input))
+  }
+}
