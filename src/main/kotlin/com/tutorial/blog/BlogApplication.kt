26bffe3ed7d4ee5f509b40c08ad8926a11216643
exmarxdespot <ofgodsandmythos@gmail.com>
2018-08-09 16:29:42 -0500

    Basic index page

--- src/main/kotlin/com/tutorial/blog/BlogApplication.kt
+++ src/main/kotlin/com/tutorial/blog/BlogApplication.kt
@@ -1,11 +1,19 @@
 package com.tutorial.blog
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class BlogApplication
 
+//fun main(args: Array<String>) {
+//  runApplication<BlogApplication>(*args)
+//}
+
+/** Customize app **/
 fun main(args: Array<String>) {
-  runApplication<BlogApplication>(*args)
+  runApplication<BlogApplication>(*args) {
+    setBannerMode(Banner.Mode.OFF)
+  }
 }
